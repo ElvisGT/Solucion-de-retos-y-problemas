074ed285b06ac1e330ab8a2a94a01fd419a5287b
Kamina <37193227+Segmentree@users.noreply.github.com>
2022-07-15 04:03:16 +0200

    create denominations hook

---
+++ Cajero_automatico/denominations.js
@@ -0,0 +1,26 @@
+let classic = [
+  {
+    cuantity: 3,
+    value: 100,
+  },
+  {
+    cuantity: 6,
+    value: 50,
+  },
+  {
+    cuantity: 10,
+    value: 20,
+  },
+  {
+    cuantity: 50,
+    value: 10,
+  },
+  {
+    cuantity: 50,
+    value: 1,
+  },
+];
+
+export function getDenominations() {
+  return { classic };
+}
