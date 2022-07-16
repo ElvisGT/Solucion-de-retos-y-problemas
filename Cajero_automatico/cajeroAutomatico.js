1b537d9cff32d23d2532e8179a10019b051b2da1
Kamina <37193227+Segmentree@users.noreply.github.com>
2022-07-16 19:47:56 +0200

    refactor cajeroAutomatico hook

--- Cajero_automatico/cajeroAutomatico.js
+++ Cajero_automatico/cajeroAutomatico.js
@@ -8,48 +8,11 @@ algoritmo es en el lenguaje JavaScript*/
   3-Obtener el valor que desea sacar el usuario(Actualmente esta establecido dentro del programa(hardcode)*/
 
 import { getDenominations } from './denominations.js';
-
-class ATM {
-  // Improvements
-  // 1 - Accumulate the change in the ATM if the user has not withdrawn all the money
-  // 2 - Create an abstract function to improve the way of the change is given
-  // 3 - Reload the amount of money of the desired denomination in the machine
-  // 4 - Create a system of login
-
-  constructor(denominations) {
-    this.denominations = denominations;
-  }
-
-  changeMoney(money) {
-    this.change = {};
-    this.denominations.forEach((denomination) => {
-      this.change[denomination.value] = 0;
-    });
-    let i = 0;
-    while (money && i < this.denominations.length) {
-      if (
-        money >= this.denominations[i].value &&
-        this.denominations[i].cuantity
-      ) {
-        this.change[this.denominations[i].value]++;
-        this.denominations[i].cuantity--;
-        money -= this.denominations[i].value;
-      } else ++i;
-    }
-  }
-
-  getChange() {
-    console.log('Please take your change:');
-    Object.keys(this.change).forEach((key) => {
-      if (this.change[key]) console.log(`${key} x ${this.change[key]}`);
-    });
-    console.log('Thanks for using our ATM');
-  }
-}
+import { basicATM } from './atm.js';
 
 const validation = (userCash) => {
   const { classic } = getDenominations();
-  let machine = new ATM(classic);
+  let machine = new basicATM(classic);
   machine.changeMoney(userCash);
   machine.getChange();
 };
